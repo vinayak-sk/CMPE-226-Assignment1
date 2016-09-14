3a723f592078fc2b53cef46fe332f26d1ed1086e
Vinayak Kalaskar <vinayak1592@gmail.com>
2016-09-13 18:18:13 -0700

    Adding database update entries in php

--- receivedata.php
+++ receivedata.php
@@ -6,8 +6,10 @@
  * Time: 4:31 PM
  */
 
-$gender = $_POST['gender'];
+$major = $_POST['major'];
 $name =  $_POST['name'];
+//$name = isset($_POST['name']) ? $_POST['name'] : '';
+$subject =  $_POST['subject'];
 
 // Create connection
 $connection = mysqli_connect("localhost", "root", "", "cmpe226", "3306");
@@ -19,7 +21,7 @@ if (!$connection) {
 }
 
 //update query
-$update_sql = "UPDATE assignment1 SET name='".$name."', gender='".$gender."' WHERE name='".$name."'";
+$update_sql = "UPDATE assignment1 SET subjects='".$subject."', major='".$major."' WHERE name='".$name."'";
 
 if (mysqli_query($connection, $update_sql)) {
     echo "Record updated successfully";
@@ -33,12 +35,46 @@ $result = mysqli_query($connection,
 
 while($row = mysqli_fetch_row($result))
 {
-    for($i=0;$i<4;$i++)
+    for($i=0;$i<10;$i++)
     {
         echo  $row[$i]."<br>";
     }
     echo "<br>";
 }
+
+//generate HTML after select query
+$query = "SELECT * FROM assignment1";
+print "<table border='1'>\n";
+                
+                // Query the database.
+                $data = $con->query($query);
+                $data->setFetchMode(PDO::FETCH_ASSOC);
+                
+                // Construct the HTML table row by row.
+                // Start with a header row.
+                $doHeader = true;
+                foreach ($data as $row) {
+                                    
+                    // The header row before the first data row.
+                    if ($doHeader) {
+                        print "        <tr>\n";
+                        foreach ($row as $name => $value) {
+                            print "            <th>$name</th>\n";
+                        }
+                        print "        </tr>\n";
+                        
+                        $doHeader = false;
+                    }
+                    
+                    // Data row.
+                    print "            <tr>\n";
+                    foreach ($row as $name => $value) {
+                        print "                <td>$value</td>\n";
+                    }
+                    print "            </tr>\n";
+                }
+                
+                print "        </table>\n";
 
 mysqli_close($connection);
 ?>
